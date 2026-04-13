3412547c4129172037c1f629f47d87c9c616e563
Saipy <147218318+Saipy404@users.noreply.github.com>
2026-04-12 21:52:49 -0700

    V1.1 GlobalSettings Update

--- my-physics-engine/Scripts/GlobalSettings.gd
+++ my-physics-engine/Scripts/GlobalSettings.gd
@@ -4,5 +4,19 @@ extends Node2D
 @export var floor = 0
 @export var gravity = Vector2(0, -9.81)
 
+@onready var gravityX = $"../GravityX"
+@onready var gravityY = $"../GravityY"
+
 func _ready():
 	Engine.max_fps = fps
+
+func _process(delta: float):
+	if gravityX.text == "":
+		gravity.x = gravity.x
+	else:
+		gravity.x = float(gravityX.text)
+		
+	if gravityY.text == "":
+		gravity.y = gravity.y
+	else:
+		gravity.y = float(gravityY.text)
